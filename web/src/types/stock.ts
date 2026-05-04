bf3300f8ef1bc4a175f74ea0b657f09e5babd765
dove.kim <kimzasqwe@naver.com>
2026-05-04 11:09:24 +0900

    웹 빌드 오류 수정

--- web/src/types/stock.ts
+++ web/src/types/stock.ts
@@ -1,6 +1,11 @@
 export type MarketType = "KOSPI" | "KOSDAQ" | "KONEX";
 export type TradingStatus = "ACTIVE" | "SUSPENDED" | "DELISTED";
 
+export interface FilterState {
+  query: string;
+  market: MarketType | "ALL";
+}
+
 export interface Stock {
   code: string;
   name: string;
